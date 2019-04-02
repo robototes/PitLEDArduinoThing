ff3f24b8ad31651d825f5b26880f9e5ba25c6d2c
OroArmor <eliorona@live.com>
2019-04-01 19:38:31 -0700

    updating to multiple strands + other optimizations

--- completed/Main/Main.ino
+++ completed/Main/Main.ino
@@ -1,40 +1,48 @@
 #include <FastLED.h>
 #define NUM_LEDS 144
 #define DATA_PIN 6
-#define hueRes NUM_LEDS
 
+#define NUM_STRANDS 3
 
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS * NUM_STRANDS];
+//CRGB allLeds[NUM_LEDS * NUM_STRANDS];
 
-CRGB RED = CRGB(255, 0, 0);
-CRGB GREEN = CRGB(0, 255, 0);
-CRGB BLUE = CRGB(0, 0, 255);
-CRGB YELLOW = CRGB(255 / 2, 255 / 2, 0);
-CRGB ORANGE = CRGB(255, 40, 0);
-CRGB CYAN = CRGB(0, 255, 200);
-CRGB PURPLE = CRGB(255 / 2, 0, 255 / 3);
-CRGB BLACK = CRGB(0, 0, 0);
-CRGB WHITE = CRGB(255, 255, 255);
+//CRGB BLUE = CRGB(0, 0, 255);
+//CRGB GREEN  = CRGB(0, 255, 0);
+//CRGB RED = CRGB(255, 0, 0);
+//CRGB YELLOW = CRGB(255 / 2, 255 / 2, 0);
+//CRGB ORANGE = CRGB(255, 40, 0);
+//CRGB CYAN = CRGB(0, 255, 200);
+//CRGB PURPLE = CRGB(255 / 2, 0, 255 / 3);
+//CRGB BLACK = CRGB(0, 0, 0);
+//CRGB WHITE = CRGB(255, 255, 255);
 
 CRGB CONSTCOLORS[8] = {
-  RED, GREEN, BLUE, YELLOW, ORANGE, CYAN, PURPLE, WHITE
+  CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::Yellow, CRGB::Orange, CRGB::Cyan, CRGB::Purple, CRGB::White
 };
 
 
 CRGB GOODCOLORS[6][3] = {
-  {GREEN, BLUE, NULL}, {RED, BLACK, NULL}, {RED, WHITE, NULL}, {WHITE, BLACK, NULL}, {PURPLE, YELLOW, NULL}, {RED, WHITE, BLUE}
+  {CRGB::Green, CRGB::Blue, NULL}, {CRGB::Red, CRGB::Black, NULL}, {CRGB::Red, CRGB::White, NULL}, {CRGB::White, CRGB::Black, NULL}, {CRGB::Purple, CRGB::Yellow, NULL}, {CRGB::Red, CRGB::White, CRGB::Blue}
 };
 
 
 
-CRGB hueColors[hueRes];
 
 void setup()
 {
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  for (int i = 0; i < hueRes; i += 1) {
-    hueColors[i] = calculateRGB(map(i,0,hueRes,0,360));
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS * NUM_STRANDS);
+  //Serial.begin(9600);
+}
+
+void show() {
+
+  for (int i = 1; i < NUM_STRANDS; i++) {
+    for (int j = 0; j < NUM_LEDS; j++) {
+      leds[NUM_LEDS * i + j] = leds[j];
+    }
   }
+  FastLED.show();
 }
 
 
@@ -42,18 +50,24 @@ void setup()
 
 void loop()
 {
-  pickRandomFunction();
+//  Serial.println(millis());
+  //pickRandomFunction();
+//  Serial.println(millis());
+  fillAll(CRGB::Blue);
+  delay(1000);
+  fillAll(CRGB::Green);
+  delay(1000);
 }
 
 void fillAll(CRGB color) {
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = color;
   }
-  FastLED.show();
+  show();
 }
 
 void pickRandomFunction() {
-  int randomPick = random(0, 6);
+  int randomPick = random(0, 7);
   //randomPick = 4;
 
   if (randomPick == 0) {
@@ -94,16 +108,22 @@ void pickRandomFunction() {
     CRGB color = CONSTCOLORS[random(sizeof(CONSTCOLORS))];
     rain(color.red, color.green, color.blue, 30, 0);
   }
+
+  else if (randomPick == 6) {
+    CRGB color = CONSTCOLORS[random(sizeof(CONSTCOLORS))];
+    smoothFade(color, 30);
+  }
 }
 
 
 void HSVFill(int d) {
   for (int i = 0; i < 360; i++) {
-    CRGB color = calculateRGB(i);
+    CRGB color;
+    hsv2rgb_spectrum(CHSV(i, 255, 255 / 2), color);
     for (int j = 0; j < NUM_LEDS; j++) {
       leds[j] = color;
     }
-    FastLED.show();
+    show();
     delay(d);
   }
 }
@@ -114,7 +134,9 @@ void HSVSwirl(int d, int repeat) { // colors to display, delay between movement,
     for (int k = 0 ; k < repeat; k++) {
       for (int j = 0; j < NUM_LEDS / repeat; j += 1) { //this loop jumps to the start of each new color segment by that length
         //leds[(i + j + k * NUM_LEDS / repeat) % NUM_LEDS] = calculateRGB((double)map(j, 0, NUM_LEDS / repeat, 0, 360));
-        leds[(i + j + k * NUM_LEDS / repeat) % NUM_LEDS] = hueColors[(int)map(j, 0, NUM_LEDS / repeat, 0, 360)];
+        //leds[(i + j + k * NUM_LEDS / repeat) % NUM_LEDS] = hueColors[(int)map(j, 0, NUM_LEDS / repeat, 0, hueRes)];
+        hsv2rgb_spectrum(CHSV(360 / (NUM_LEDS / repeat), 255, 255 / 2), leds[(i + j + k * NUM_LEDS / repeat) % NUM_LEDS]);
+
       }
     }
     FastLED.show();
@@ -122,69 +144,6 @@ void HSVSwirl(int d, int repeat) { // colors to display, delay between movement,
   }
 }
 
-CRGB calculateRGB(double angle) {
-
-  if (angle < 0) {
-    angle = 0;
-  }
-
-  double l = 0.5;
-  double s = 1.0;
-
-  double temp1 = l + s - l * s;
-  double temp2 = 2.0 * l - temp1;
-
-  double hue = angle / 360.0;
-  double tempRed = hue + 0.333;
-  double tempGreen = hue;
-  double tempBlue = hue - 0.333;
-
-  if (tempRed > 1) {
-    tempRed -= 1;
-  }
-
-  if (tempBlue < 0) {
-    tempBlue += 1;
-  }
-
-  double red = 0;
-  Serial.println(tempRed);
-  if (6 * tempRed < 1) {
-    red = temp2 + (temp1 - temp2) * 6.0 * tempRed;
-  } else if (2 * tempRed < 1) {
-    red = temp1;
-  } else if (3 * tempRed < 2 ) {
-    red = temp2 + (temp1 - temp2) * (0.666 - tempRed) * 6.0;
-  } else {
-    red = temp2;
-  }
-
-  double green = 0;
-
-  if (6 * tempGreen < 1) {
-    green = temp2 + (temp1 - temp2) * 6.0 * tempGreen;
-  } else if (2 * tempGreen < 1) {
-    green = temp1;
-  } else if (3 * tempGreen < 2 ) {
-    green = temp2 + (temp1 - temp2) * (0.666 - tempGreen) * 6.0;
-  } else {
-    green = temp2;
-  }
-
-  double blue = 0;
-
-  if (6 * tempBlue < 1) {
-    blue = temp2 + (temp1 - temp2) * 6.0 * tempBlue;
-  } else if (2 * tempBlue < 1) {
-    blue = temp1;
-  } else if (3 * tempBlue < 2 ) {
-    blue = temp2 + (temp1 - temp2) * (0.666 - tempBlue) * 6.0;
-  } else {
-    blue = temp2;
-  }
-  return CRGB(red * 255, green * 255, blue * 255);
-
-}
 
 
 void swirl( CRGB colors[], int numColors,  int d, int repeat) { // colors to display, delay between movement, number of times each color shows up. try to make colors * repeat  = 144.
@@ -198,7 +157,7 @@ void swirl( CRGB colors[], int numColors,  int d, int repeat) { // colors to dis
       }
       currentColor++;
     }
-    FastLED.show();
+    show();
     delay(d);
   }
 }
@@ -211,7 +170,7 @@ void sweep(byte red, byte green, byte blue, int d, int times, int startPoint)
     {
       leds[(dot + i * NUM_LEDS / times + startPoint) % NUM_LEDS] = CRGB( red, green, blue); // set this pixel to color, mod is to be safe
     }
-    FastLED.show();
+    show();
     delay(d);
   }
 }
@@ -226,7 +185,7 @@ void randomFade(byte red, byte green, byte blue, int d)
       dotRemaining --;
     } else {
       leds[randomSelection] = CRGB( red, green, blue); // set this pixel to color
-      FastLED.show();
+      show();
       delay(d);
     }
   }
@@ -237,32 +196,40 @@ void rain(byte red, byte green, byte blue, int d, int center)
   for (int dot = 0; dot < NUM_LEDS / 2; dot++)
   {
     leds[(center + dot) % NUM_LEDS] = CRGB( red, green, blue); // set this pixel to color
-    leds[(center - dot) % 144] = CRGB(red, green, blue);
-    FastLED.show();
+    if (center - dot < 0) {
+      leds[(center - dot + NUM_LEDS) % NUM_LEDS] = CRGB(red, green, blue);
+    } else {
+      leds[(center - dot) % NUM_LEDS] = CRGB(red, green, blue);
+    }
+
+    show();
     delay(d);
   }
 }
 
 double lerp(int from, int to, double percent) {
-  return to+ (to - from) * percent;
+  return from + (to - from) * percent;
 }
 
+CRGB lerp(CRGB from, CRGB to, double percent) {
+  return CRGB(
+           (int)lerp(from.red, to.red, percent),
+           (int)lerp(from.green, to.green, percent),
+           (int)lerp(from.blue, to.blue, percent)
+         );
+}
 
 void smoothFade(CRGB toColor, int d) {
-  CRGB oldLeds[NUM_LEDS] = leds;
-//  for (int i = 0; i < NUM_LEDS; i++) {
-//    oldLeds[i] = leds[i];
-//  }
+  CRGB oldLeds[NUM_LEDS];
+  for (int i = 0; i < NUM_LEDS; i++) {
+    oldLeds[i] = leds[i];
+  }
 
   for (double i = 0; i < 1; i += 0.01) {
     for (int j = 0; j < NUM_LEDS; j++) {
-//      CRGB oldColor = oldLeds[i];
-//      CRGB newColor = CRGB(
-//                  lerp(oldColor.red, toColor.red, i),
-//                  lerp(oldColor.green, toColor.green, i),
-//                  lerp(oldColor.blue, toColor.blue, i)
-//                ); 
-//      leds[i] = newColor;
+      leds[j] = lerp(oldLeds[j], toColor, i);
     }
+    delay(d);
+    show();
   }
 }
